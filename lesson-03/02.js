f3d462e11c4fb363ed3726d1fa507d18604b8d9a
Nikita <mikhaylovnikita05@gmail.com>
2025-08-27 19:18:13 +0300

    task-3

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -1,4 +1,11 @@
 // Разработайте функцию isEven, которая определяет, чётное ли число.
 // Функция должна возвращать true, если число чётное, и false — в противном случае
 
-function isEven() {}
+function isEven(chislo) {
+    if(chislo % 2 === 0) {
+    return true;
+    }
+    else{
+        return false;
+    }
+}
--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,3 +1,17 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
-function findLargest() {}
+function findLargest(q1, q2, q3) {
+    let qq;
+    if (q1 > q2) {
+        qq = q1
+    }
+    else{
+        qq = q2
+    }
+    if (qq > q3) {
+        return qq;
+    }
+    else {
+        return q3;
+    }
+}
