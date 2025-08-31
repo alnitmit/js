ad5fafd2e236ec4906b5ebca2e53e30d173f153a
Nikita <mikhaylovnikita05@gmail.com>
2025-08-31 18:43:42 +0300

    task4

--- lesson-04/02.js
+++ lesson-04/02.js
@@ -15,4 +15,13 @@
 Подсказка: можно использовать функцию `includesElement`, которую мы написали ранее. Переписывать её не нужно, она доступна по всему проекту за счёт hoisting.
 */
 
-function findUniqueElements() {}
+function findUniqueElements(array) {
+    const arr = [];
+    for (let i = 0; i < array.length; i++) {
+        let istrue = includesElement(arr, array[i]);
+        if (!istrue) {
+            arr.push(array[i]);
+        }
+    }
+    return arr;
+}
--- lesson-04/03.js
+++ lesson-04/03.js
@@ -17,4 +17,15 @@ findCommonElements([1, 2, 3], [2, 3, 4]) // [2, 3]
 Подсказка: можно использовать функцию `includesElement`, которую мы написали ранее. Переписывать её не нужно, она доступна по всему проекту за счёт hoisting.
 */
 
-function findCommonElements() {}
+function findCommonElements(array1, array2) {
+    const arr = [];
+    for( let i = 0; i < array1.length; i++) {
+        for( let j = 0; j < array2.length; j++) {
+            if (array1[i] === array2 [j]) {
+                arr.push(array1[i]);
+                break
+            }
+        }
+    }
+    return arr;
+}
