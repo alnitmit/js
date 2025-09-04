509fcae81bde77e135797ea4e0f65a86a3d34864
Nikita <mikhaylovnikita05@gmail.com>
2025-09-04 22:07:01 +0300

    a

--- lesson-05/01.js
+++ lesson-05/01.js
@@ -11,9 +11,16 @@
 */
 
 const game = {
-  resources: {
-    gold: 250,
-    lumber: 100,
-  },
-  addResource() {}
+    resources: {
+        gold: 250,
+        lumber: 100,
+    },
+    addResource(resource, amount) {
+        if (this.resources.hasOwnProperty(resource)) {
+            this.resources[resource] += amount;
+        }
+        else {
+            console.log("Invalid resource");
+        }
+    }
 }
